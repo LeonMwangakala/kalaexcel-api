02005f859471995c93d6fe72931be7573ee5670b
Leon Mwangakala <leon.mwangakala@nssf.go.tz>
2026-01-09 15:14:43 +0300

    Add CORS override in AppServiceProvider boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Override CORS headers in response after it's prepared
+        $this->app['events']->listen(\Illuminate\Http\Events\ResponsePrepared::class, function ($event) {
+            $request = $event->request;
+            $response = $event->response;
+            $origin = $request->header('Origin');
+            
+            $allowedOrigins = ['https://core.kalaexcel.com', 'https://www.kalaexcel.com', 'https://kalaexcel.com'];
+            
+            if ($origin && in_array($origin, $allowedOrigins)) {
+                // Remove wildcard header completely
+                $response->headers->remove('Access-Control-Allow-Origin');
+                // Set specific origin
+                $response->headers->set('Access-Control-Allow-Origin', $origin);
+                $response->headers->set('Access-Control-Allow-Credentials', 'true');
+            }
+        });
     }
 }
