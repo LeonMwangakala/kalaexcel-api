fd6078a4860fa23d2ec8f9de83883aa07acdf5bd
Leon Mwangakala <leon.mwangakala@nssf.go.tz>
2026-01-09 15:04:08 +0300

    Remove withEvents from bootstrap/app.php - use service provider instead

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,25 +27,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })
-    ->withEvents(function ($events): void {
-        // Override CORS headers after response is prepared to ensure no wildcard
-        $events->listen(\Illuminate\Http\Events\ResponsePrepared::class, function ($event) {
-            $request = $event->request;
-            $response = $event->response;
-            $origin = $request->header('Origin');
-            
-            $allowedOrigins = [
-                'https://core.kalaexcel.com',
-                'https://www.kalaexcel.com',
-                'https://kalaexcel.com',
-            ];
-            
-            if ($origin && in_array($origin, $allowedOrigins)) {
-                // Force remove wildcard and set specific origin
-                $response->headers->remove('Access-Control-Allow-Origin');
-                $response->headers->set('Access-Control-Allow-Origin', $origin, false);
-                $response->headers->set('Access-Control-Allow-Credentials', 'true', false);
-            }
-        });
     })->create();
